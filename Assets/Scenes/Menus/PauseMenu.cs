b25bbd16160bd03860722a7b8087431663feb22c
TomatoesAreGood <139704217+TomatoesAreGood@users.noreply.github.com>
2023-10-08 09:54:41 -0400

    PauseMenu and Scuffed StartMenu

---
+++ Assets/Scenes/Menus/PauseMenu.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool isPaused = false;
+
+    public GameObject pauseMenuUI;
+
+
+    // Update is called once per frame
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)){
+            if(isPaused){
+                Resume();
+            }else{
+                Pause();
+            }
+        }
+    }
+
+    private void Resume(){
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        isPaused = false;
+    }
+
+    private void Pause(){
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        isPaused = true;
+    }
+    
+    public void LoadMenu(){
+        SceneManager.LoadScene("StartMenu");
+        if(Time.timeScale == 0){
+            Resume();
+        }
+    }
+    public void Quit(){
+        Debug.Log("quitted game");
+        Application.Quit();
+    }
+    public void ResumeGame(){
+        Resume();
+    }
+}
